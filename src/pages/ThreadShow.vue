49970391df5629a7b707848ceb5ed8cef376af70
YEVHEN_ZAKHARCH <Y.Zakharchenko@steel.mil.ua>
2024-05-01 12:50:14 +0300

    PostEditor done

--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -3,42 +3,27 @@
     <h1>{{ thread.title }}</h1>
 
     <post-list :posts="threadPosts" />
-
-    <div class="col-full">
-      <form @submit.prevent="addPost">
-        <div class="form-group">
-          <textarea v-model="newPostText"
-          id="" cols="30" rows="10" class="form-input"/>
-          
-        </div>
-        <div class="form-actions">
-          <button class="btn-blue">
-            Submit post
-          </button>
-        </div>
-      
-    </form>
-    </div>
+    <post-editor @save="addPost"/>
   </div>
 </template>
 
 <script>
 import PostList from '@/components/PostList.vue'
+import PostEditor from '@/components/PostEditor.vue'
 import sourceData from '@/data.json'
 export default {
   name: 'ThreadShow',
-  components: { PostList },
+  components: { PostList, PostEditor },
   props: {
     id: {
       required: true,
       type: String
     }
   },
-  data() {
+  data () {
     return {
       threads: sourceData.threads,
-      posts: sourceData.posts,
-      newPostText: ''
+      posts: sourceData.posts
     }
   },
   computed: {
@@ -50,19 +35,18 @@ export default {
     }
   },
   methods: {
-    addPost() {
-      const postId = 'slavaUkraini' + Math.random()
+    addPost(eventData) {
+      // const postId = 'slavaUkraini' + Math.random()
       const post = {
-        id: postId,
-        text: this.newPostText,
-        publishedAt: Math.floor(Date.now() / 1000),
+        ...eventData.post,
+        // id: postId,
+        // text: this.newPostText,
+        // publishedAt: Math.floor(Date.now() / 1000),
         threadId: this.id,
-        userId: '38St7Q8Zi2N1SPa5ahzssq9kbyp1'
+        // userId: '38St7Q8Zi2N1SPa5ahzssq9kbyp1'
       }
       this.posts.push(post)
-      this.thread.posts.push(postId)
-
-      this.newPostText = ''
+      this.thread.posts.push(post.id)
     }  
   }
 }
